962b9b99bb7092f66c930ab04add0570edefe329
corryws <corradotrigilia97@gmail.com>
2023-08-21 20:44:40 +0200

    Fixed Detect Ai,Add Spark Folder

--- DetectAI/Detect.py
+++ DetectAI/Detect.py
@@ -1,6 +1,7 @@
-from model import GPT2PPLV2
-model = GPT2PPLV2()
-sentence = "your text here"
-model(sentence, "number of words per chunk", "v1.1")
+from model import GPT2PPL
+model = GPT2PPL()
 
-print(model)
+#Inserire qui, le varie descrizioni/recensioni date dallo scraper
+
+sentence = ""
+model(sentence)
