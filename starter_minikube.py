3a1028ac2fc87fdea83fa1d6e39a0fc181c4f72e
Gayan Seneviratne <gasm98@hotmail.it>
2023-09-16 17:11:13 +0200

    Fixed starter_minikube

--- starter_minikube.py
+++ starter_minikube.py
@@ -4,8 +4,8 @@ import os
 #go to the directory /data_ingestion/logstash and run the command: docker build . -t tap:logstash
 
 os.system("minikube image build ./logstash -t tap:logstash")
-os.system("minikube image build ./kafka -t tap:kafka")
-os.system("minikube image build ./spark -t tap:spark")
+os.system("minikube image build ./kubernetes/kafka -t tap:kafka")
+os.system("minikube image build ./kubernetes/spark -t tap:spark")
 os.system("minikube image build ./kibana -t tap:kibana")
 os.system("minikube image build ./Scraper -t tap:scraper")
 #os.system("minikube build ./DetectAI -t tap:detectai")
